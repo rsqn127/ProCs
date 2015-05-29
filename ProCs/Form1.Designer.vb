860af3e4b23bb8faf767fb73c6b5aa2167088560
esc :-P <segoist43@redns.org>
2015-05-30 01:56:30 +0900

    initial commit

---
+++ ProCs/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.lvProcess = New System.Windows.Forms.ListView()
+        Me.chName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chPid = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chStatus = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chMemory = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnKill = New System.Windows.Forms.Button()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lvProcess
+        '
+        Me.lvProcess.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chName, Me.chPid, Me.chStatus, Me.chMemory})
+        Me.lvProcess.FullRowSelect = True
+        Me.lvProcess.GridLines = True
+        resources.ApplyResources(Me.lvProcess, "lvProcess")
+        Me.lvProcess.Name = "lvProcess"
+        Me.lvProcess.UseCompatibleStateImageBehavior = False
+        Me.lvProcess.View = System.Windows.Forms.View.Details
+        '
+        'chName
+        '
+        resources.ApplyResources(Me.chName, "chName")
+        '
+        'chPid
+        '
+        resources.ApplyResources(Me.chPid, "chPid")
+        '
+        'chStatus
+        '
+        resources.ApplyResources(Me.chStatus, "chStatus")
+        '
+        'chMemory
+        '
+        resources.ApplyResources(Me.chMemory, "chMemory")
+        '
+        'btnKill
+        '
+        resources.ApplyResources(Me.btnKill, "btnKill")
+        Me.btnKill.Name = "btnKill"
+        Me.btnKill.UseVisualStyleBackColor = True
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 2500
+        '
+        'frmMain
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.btnKill)
+        Me.Controls.Add(Me.lvProcess)
+        Me.Name = "frmMain"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lvProcess As System.Windows.Forms.ListView
+    Friend WithEvents chName As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chPid As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chStatus As System.Windows.Forms.ColumnHeader
+    Friend WithEvents chMemory As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnKill As System.Windows.Forms.Button
+    Friend WithEvents Timer As System.Windows.Forms.Timer
+
+End Class
---
+++ ProCs/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class frmMain
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ProcessView()
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        ProcessView()
+    End Sub
+
+    Private Sub ProcessView()
+        Me.lvProcess.Items.Clear()
+        For Each proc In Process.GetProcesses()
+            Dim Str = ""
+            If (proc.Responding) Then
+                Str = "실행"
+            Else
+                Str = "중지"
+            End If
+            Dim strArray = New String() {proc.ProcessName.ToString(), proc.Id.ToString(), Str, proc.WorkingSet64.ToString()}
+            Dim lvt = New ListViewItem(strArray)
+            Me.lvProcess.Items.Add(lvt)
+        Next
+    End Sub
+
+    Private Sub btnKill_Click(sender As Object, e As EventArgs) Handles btnKill.Click
+        Try
+            Dim PName = Me.lvProcess.SelectedItems(0).SubItems(0).Text
+            Dim tProcess = Process.GetProcessesByName(PName)
+            If (tProcess.Length = 1) Then
+                Dim dlr = MessageBox.Show(PName + " 프로세스를 끝내시겠습니까?", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+                If (dlr = DialogResult.Yes) Then
+                    tProcess(0).Kill()
+                    ProcessView()
+                End If
+            Else
+
+                MessageBox.Show(Me.lvProcess.SelectedItems(0).SubItems(0).Text + "프로세스는 존재하지 않습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                ProcessView()
+            End If
+        Catch
+            Return
+        End Try
+    End Sub
+End Class
